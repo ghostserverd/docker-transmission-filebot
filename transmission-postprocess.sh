fdecf5e844f7266eeccac4b3f8bddb837cc435ab
ghostserverd <42226464+ghostserverd@users.noreply.github.com>
2019-04-07 14:12:22 -0700

    change filebot action to move

--- transmission-postprocess.sh
+++ transmission-postprocess.sh
@@ -8,4 +8,4 @@ ARG_LABEL="N/A"
 # Configuration
 CONFIG_OUTPUT="/media"
 
-/usr/local/bin/filebot -script fn:amc --output "$CONFIG_OUTPUT" --action duplicate --conflict auto -non-strict --log-file /media/amc.log --def unsorted=y music=y artwork=y subtitles=en movieFormat={plex} seriesFormat={plex} animeFormat={plex} musicFormat={plex} excludeList=".excludes" ut_dir="$ARG_PATH" ut_kind="multi" ut_title="$ARG_NAME" ut_label="$ARG_LABEL"
+/usr/local/bin/filebot -script fn:amc --output "$CONFIG_OUTPUT" --action move --conflict auto -non-strict --log-file /media/amc.log --def unsorted=y music=y artwork=y subtitles=en movieFormat={plex} seriesFormat={plex} animeFormat={plex} musicFormat={plex} excludeList=".excludes" ut_dir="$ARG_PATH" ut_kind="multi" ut_title="$ARG_NAME" ut_label="$ARG_LABEL"
